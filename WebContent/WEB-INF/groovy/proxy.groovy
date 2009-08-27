d6c3794e154edcf14e18a2d7e7b94cb26e9f0f1a
bfritsch <bfritsch@0788afb5-04a5-434c-9def-4119914b379a>
2009-08-27 13:21:20 +0000

    fixed url
    git-svn-id: https://isisvn.unil.ch/svn/hecwall/itunesu@39 0788afb5-04a5-434c-9def-4119914b379a

--- WebContent/WEB-INF/groovy/proxy.groovy
+++ WebContent/WEB-INF/groovy/proxy.groovy
@@ -1,6 +1,6 @@
 if(request.getParameter("url")){
 	def url = request.getParameter("url")
-	def conn = "http://deimos.apple.com/WebObjects/Core.woa/DownloadRedirectedTrackPreview/unil.ch.2244181544.02244181550.2374720214.m4v".toURL().openConnection()
+	def conn = url.toURL().openConnection()
 	conn.setRequestProperty ( "User-Agent", "iTunes/8.1" )
 	conn.getHeaderFields()
 	response.sendRedirect( ( conn.getURL() =~ /http:\/\/deimos3.apple.com\/DeliverPreview/).replaceFirst("http:/") )	
